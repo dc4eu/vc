c3ef2d7d49719d71ff52947e032920e8f3120d97
masv3971 <masv@sunet.se>
2023-10-19 09:41:12 +0200

    Remove not public endpoints.

--- internal/registry/httpserver/api.go
+++ internal/registry/httpserver/api.go
@@ -9,8 +9,6 @@ import (
 
 // Apiv1 interface
 type Apiv1 interface {
-	//Add(ctx context.Context, req *apiv1_registry.AddRequest) (*apiv1_registry.AddReply, error)
-	//Revoke(ctx context.Context, req *apiv1_registry.RevokeRequest) (*apiv1_registry.RevokeReply, error)
 	Validate(ctx context.Context, req *apiv1_registry.ValidateRequest) (*apiv1.ValidateReply, error)
 
 	Status(ctx context.Context, req *apiv1_status.StatusRequest) (*apiv1_status.StatusReply, error)

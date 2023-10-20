15c2c92d88beb3a0fcbea881170e9ce4370cbdd6
masv3971 <masv@sunet.se>
2023-10-20 17:08:45 +0200

    Parameterized mongo query.

--- internal/datastore/db/methods.go
+++ internal/datastore/db/methods.go
@@ -17,7 +17,9 @@ type Coll struct {
 
 func (c *Coll) createIndex(ctx context.Context) error {
 	indexModel := mongo.IndexModel{
-		Keys: bson.M{"document_id": 1},
+		Keys: bson.M{
+			"document_id": bson.M{"$eq": 1},
+		},
 	}
 	_, err := c.Coll.Indexes().CreateOne(ctx, indexModel)
 	if err != nil {
@@ -35,14 +37,14 @@ func (c *Coll) Save(ctx context.Context, doc *model.Upload) error {
 // IDMapping return authentic source person id if any
 func (c *Coll) IDMapping(ctx context.Context, query *model.MetaData) (string, error) {
 	filter := bson.M{
-		"meta.authentic_source": query.AuthenticSource,
-		"meta.collect_id":       query.CollectID,
-		"meta.document_type":    query.DocumentType,
-		"meta.document_id":      query.DocumentID,
-		"meta.uid":              query.UID,
-		"meta.date_of_birth":    query.DateOfBirth,
-		"meta.last_name":        query.LastName,
-		"meta.first_name":       query.FirstName,
+		"meta.authentic_source": bson.M{"$eq": query.AuthenticSource},
+		"meta.collect_id":       bson.M{"$eq": query.CollectID},
+		"meta.document_type":    bson.M{"$eq": query.DocumentType},
+		"meta.document_id":      bson.M{"$eq": query.DocumentID},
+		"meta.uid":              bson.M{"$eq": query.UID},
+		"meta.date_of_birth":    bson.M{"$eq": query.DateOfBirth},
+		"meta.last_name":        bson.M{"$eq": query.LastName},
+		"meta.first_name":       bson.M{"$eq": query.FirstName},
 	}
 	opts := options.FindOne().SetProjection(bson.M{"meta.authentic_source_person_id": 1})
 	var res string
@@ -55,9 +57,9 @@ func (c *Coll) IDMapping(ctx context.Context, query *model.MetaData) (string, er
 // GetDocument return matching document if any, or error
 func (c *Coll) GetDocument(ctx context.Context, attr *model.MetaData) (*model.Upload, error) {
 	filter := bson.M{
-		"meta.authentic_source": attr.AuthenticSource,
-		"meta.document_type":    attr.DocumentType,
-		"meta.document_id":      attr.DocumentID,
+		"meta.authentic_source": bson.M{"$eq": attr.AuthenticSource},
+		"meta.document_type":    bson.M{"$eq": attr.DocumentType},
+		"meta.document_id":      bson.M{"$eq": attr.DocumentID},
 	}
 	opt := options.FindOne().SetProjection(bson.M{
 		"meta.revocation_id": 1,
@@ -75,14 +77,14 @@ func (c *Coll) GetDocument(ctx context.Context, attr *model.MetaData) (*model.Up
 // GetDocumentByCollectID return matching document if any, or error
 func (c *Coll) GetDocumentByCollectID(ctx context.Context, query *model.MetaData) (*model.Upload, error) {
 	filter := bson.M{
-		"meta.authentic_source": query.AuthenticSource,
-		"meta.collect_id":       query.CollectID,
-		"meta.document_type":    query.DocumentType,
-		"meta.document_id":      query.DocumentID,
-		"meta.uid":              query.UID,
-		"meta.date_of_birth":    query.DateOfBirth,
-		"meta.last_name":        query.LastName,
-		"meta.first_name":       query.FirstName,
+		"meta.authentic_source": bson.M{"$eq": query.AuthenticSource},
+		"meta.collect_id":       bson.M{"$eq": query.CollectID},
+		"meta.document_type":    bson.M{"$eq": query.DocumentType},
+		"meta.document_id":      bson.M{"$eq": query.DocumentID},
+		"meta.uid":              bson.M{"$eq": query.UID},
+		"meta.date_of_birth":    bson.M{"$eq": query.DateOfBirth},
+		"meta.last_name":        bson.M{"$eq": query.LastName},
+		"meta.first_name":       bson.M{"$eq": query.FirstName},
 	}
 
 	opt := options.FindOne().SetProjection(bson.M{
@@ -103,8 +105,8 @@ func (c *Coll) GetDocumentByCollectID(ctx context.Context, query *model.MetaData
 // ListMetadata returns a list of information to be used for geting documents
 func (c *Coll) ListMetadata(ctx context.Context, query *model.MetaData) ([]model.MetaData, error) {
 	filter := bson.M{
-		"meta.authentic_source":           query.AuthenticSource,
-		"meta.authentic_source_person_id": query.AuthenticSourcePersonID,
+		"meta.authentic_source":           bson.M{"$eq": query.AuthenticSource},
+		"meta.authentic_source_person_id": bson.M{"$eq": query.AuthenticSourcePersonID},
 	}
 	opt := options.Find().SetProjection(
 		bson.M{

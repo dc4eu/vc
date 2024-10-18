26f0446f203acc9be4f943ce7d9a1d8724d3179b
masv3971 <masv@sunet.se>
2024-10-18 09:25:17 +0200

    Types needs to be exported, want to test the array not only first instance of it.

--- pkg/helpers/validate_test.go
+++ pkg/helpers/validate_test.go
@@ -88,45 +88,97 @@ var identity = &model.Identity{
 	BirthDate: "1970-01-01",
 }
 
-func TestValidationArrayOfIdentity(t *testing.T) {
+func TestStruct(t *testing.T) {
+	type Name struct {
+		First string `validate:"required"`
+		Last  string `validate:"required"`
+	}
+	type myStruct struct {
+		Names []Name `validate:"dive"`
+	}
+
 	tts := []struct {
 		name string
-		have []model.Identity `validate:"dive"`
+		have myStruct
 		want error
 	}{
 		{
-			name: "Correct datetime format",
-			have: []model.Identity{
-				{
-					Schema: &model.IdentitySchema{
-						Name: "SE",
+			name: "empty",
+			have: myStruct{
+				Names: []Name{
+					{
+						First: "John",
 					},
-					BirthDate: "1972-10-27",
-				},
-			},
-			want: nil,
-		},
-		{
-			name: "wrong datetime format",
-			have: []model.Identity{
-				{
-					Schema: &model.IdentitySchema{
-						Name: "SE",
-					},
-					BirthDate: "1972-10-27 10:15:31.432635902 +0000 UTC",
 				},
 			},
 			want: &Error{
 				Title: "validation_error",
 				Details: []map[string]interface{}{
 					{
-						"field":           "birth_date",
-						"namespace":       "birth_date",
+						"field":           "Last",
+						"namespace":       "Names[0].Last",
 						"type":            "string",
-						"validation":      "datetime",
-						"validationParam": "2006-01-02",
-						"value":           "1972-10-27 10:15:31.432635902 +0000 UTC",
+						"validation":      "required",
+						"validationParam": "",
+						"value":           "",
 					},
+				},
+			},
+		},
+	}
+	for _, tt := range tts {
+		t.Run(tt.name, func(t *testing.T) {
+			got := CheckSimple(tt.have)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestValidationArrayOfIdentity(t *testing.T) {
+	type myStruct struct {
+		ID []model.Identity `validate:"dive"`
+	}
+	tts := []struct {
+		name string
+		Have myStruct
+		want error
+	}{
+		{
+			name: "Correct datetime format",
+			Have: myStruct{
+				ID: []model.Identity{
+					{
+						Schema: &model.IdentitySchema{
+							Name: "SE",
+						},
+						BirthDate: "1972-10-27",
+					},
+				},
+			},
+			want: nil,
+		},
+		{
+			name: "wrong datetime format",
+			Have: myStruct{
+				ID: []model.Identity{
+					{
+						Schema: &model.IdentitySchema{
+							Name: "SE",
+						},
+						BirthDate: "1972-10-27 10:15:31.432635902 +0000 UTC",
+					},
+				},
+			},
+			want: &Error{
+				Title: "validation_error",
+				Details: []map[string]interface{}{{
+					"field":           "birth_date",
+					"namespace":       "ID[0].birth_date",
+					"type":            "string",
+					"validation":      "datetime",
+					"validationParam": "2006-01-02",
+					"value":           "1972-10-27 10:15:31.432635902 +0000 UTC",
+				},
 				},
 			},
 		},
@@ -134,7 +186,7 @@ func TestValidationArrayOfIdentity(t *testing.T) {
 
 	for _, tt := range tts {
 		t.Run(tt.name, func(t *testing.T) {
-			got := CheckSimple(tt.have[0])
+			got := CheckSimple(tt.Have)
 			assert.Equal(t, tt.want, got)
 		})
 	}

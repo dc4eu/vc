19c3bc56e22e86a63a88ff1f98ac0f281cc5e5bf
Mats Kramer <mjnkramer@gmail.com>
2024-10-30 18:47:54 +0100

    Ver: Now handles the "new" credential format: header.payload.signature~disclosure1~...~ as well as the old format without disclosures in the end. More readable code.

--- internal/verifier/apiv1/handler.go
+++ internal/verifier/apiv1/handler.go
@@ -16,14 +16,19 @@ import (
 	"vc/pkg/model"
 )
 
-// Status return status for each instance
-func (c *Client) Status(ctx context.Context, req *apiv1_status.StatusRequest) (*apiv1_status.StatusReply, error) {
-	probes := model.Probes{}
-	return probes.Check("verifier"), nil
-}
+const (
+	ErrInvalidJWTStructure = "invalid JWT structure, expected header.payload.signature"
+	ErrInvalidPayloadJSON  = "failed to parse payload JSON"
+	ErrInvalidJWKField     = "missing or invalid JWK field"
+	ErrTokenVerification   = "error verifying token"
+	ErrInvalidToken        = "invalid token"
+	ErrInvalidClaims       = "invalid claims"
+	ErrExpiredToken        = "token has expired"
+	ErrNotYetValidToken    = "token is not valid yet"
+)
 
 type VerifyCredentialRequest struct {
-	Jwt string `json:"jwt" validate:"required"`
+	Credential string `json:"credential" validate:"required"`
 }
 
 type VerifyCredentialReply struct {
@@ -31,10 +36,12 @@ type VerifyCredentialReply struct {
 	Message string `json:"message,omitempty"`
 }
 
-type JWTParts struct {
-	Header    string
-	Payload   string
-	Signature string
+type SDJWTParts struct {
+	CompleteJWT string
+	Header      string
+	Payload     string
+	Signature   string
+	Disclosures []string
 }
 
 type JWK struct {
@@ -44,54 +51,143 @@ type JWK struct {
 	Y   string `json:"y"`
 }
 
-type PayloadCnf struct {
-	Cnf struct {
-		Jwk JWK `json:"jwk"`
-	} `json:"cnf"`
+// Status returns the status for each instance.
+func (c *Client) Status(ctx context.Context, req *apiv1_status.StatusRequest) (*apiv1_status.StatusReply, error) {
+	probes := model.Probes{}
+	return probes.Check("verifier"), nil
 }
 
+// VerifyCredential verifies a credential (sd-jwt currently supported)
 func (c *Client) VerifyCredential(ctx context.Context, request *VerifyCredentialRequest) (*VerifyCredentialReply, error) {
-	jwtParts, err := splitJWT(request.Jwt)
+	//TODO(mk): ev. avgör snabbt om det är en sd-jwt eller någon annan typ av credential?
+
+	sdjwtParts, err := splitCredential(request.Credential)
 	if err != nil {
-		msg := "Invalid JWT structure, unable to split into header, payload and signature"
-		c.log.Debug(msg, "err", err)
-		return &VerifyCredentialReply{Valid: false, Message: msg}, nil
+		return c.createInvalidReply(ErrInvalidJWTStructure, err)
+	}
+	c.log.Debug("credential", "parts", sdjwtParts)
+
+	jwk, err := extractJWK(sdjwtParts.Payload)
+	if err != nil {
+		return c.createInvalidReply(ErrInvalidJWKField, err)
+	}
+	c.log.Debug("jwk", "data", jwk)
+
+	pubKey, err := createPubKey(jwk)
+	if err != nil {
+		return c.createInvalidReply(ErrInvalidJWKField, err)
+	}
+	c.log.Debug("pubkey", "data", pubKey)
+
+	token, err := parseJWT(sdjwtParts.CompleteJWT, pubKey)
+	if err != nil {
+		return c.createInvalidReply(ErrTokenVerification, err)
+	}
+	c.log.Debug("token", "data", token)
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		if err := validateClaims(claims); err != nil {
+			return c.createInvalidReply(ErrInvalidClaims, err)
+		}
+		c.logClaims(claims)
+		//TODO(mk): validate disclosures in any way?
+		return &VerifyCredentialReply{Valid: true}, nil
 	}
 
-	c.log.Debug("jwt", "header", jwtParts.Header)
-	c.log.Debug("jwt", "payload", jwtParts.Payload)
-	c.log.Debug("jwt", "signature", jwtParts.Signature)
+	return c.createInvalidReply(ErrInvalidToken, err)
+}
 
-	//fmt.Println("Header:", jwtParts.Header)
-	//fmt.Println("Payload:", jwtParts.Payload)
-	//fmt.Println("Signature:", jwtParts.Signature)
+func (c *Client) createInvalidReply(message string, err error) (*VerifyCredentialReply, error) {
+	c.log.Debug(message, "err", err)
+	return &VerifyCredentialReply{Valid: false, Message: message}, nil
+}
 
-	var payloadCnf PayloadCnf
-	if err := json.Unmarshal([]byte(jwtParts.Payload), &payloadCnf); err != nil {
-		msg := "Failed to parse payload JSON"
-		c.log.Debug(msg, "err", err)
-		return &VerifyCredentialReply{Valid: false, Message: msg}, nil
+func (c *Client) logClaims(claims jwt.MapClaims) {
+	c.log.Debug("Token is valid. Claims:")
+	for key, val := range claims {
+		c.log.Debug("claim", "key", key, "val", val)
+	}
+}
+
+func splitCredential(credential string) (*SDJWTParts, error) {
+	parts := strings.Split(credential, "~")
+	jwtParts := strings.Split(parts[0], ".")
+	if len(jwtParts) != 3 {
+		return nil, errors.New(ErrInvalidJWTStructure)
 	}
 
-	xStr := payloadCnf.Cnf.Jwk.X
-	yStr := payloadCnf.Cnf.Jwk.Y
-
-	c.log.Debug("jwt", "x", xStr, "y", yStr)
-
-	//fmt.Println("JWK x:", xStr)
-	//fmt.Println("JWK y:", yStr)
-
-	xBytes, err := base64.RawURLEncoding.DecodeString(xStr)
+	header, err := decodeBase64URL(jwtParts[0])
 	if err != nil {
-		msg := "Error decoding x"
-		c.log.Debug(msg, "err", err)
-		return &VerifyCredentialReply{Valid: false, Message: msg}, nil
+		return nil, err
 	}
-	yBytes, err := base64.RawURLEncoding.DecodeString(yStr)
+
+	payload, err := decodeBase64URL(jwtParts[1])
 	if err != nil {
-		msg := "Error decoding y"
-		c.log.Debug(msg, "err", err)
-		return &VerifyCredentialReply{Valid: false, Message: msg}, nil
+		return nil, err
+	}
+
+	return &SDJWTParts{
+		CompleteJWT: parts[0],
+		Header:      header,
+		Payload:     payload,
+		Signature:   jwtParts[2],
+		Disclosures: parts[1:],
+	}, nil
+}
+
+func extractJWK(payload string) (*JWK, error) {
+	var payloadMap map[string]interface{}
+	if err := json.Unmarshal([]byte(payload), &payloadMap); err != nil {
+		return nil, errors.New(ErrInvalidPayloadJSON)
+	}
+
+	cnf, ok := payloadMap["cnf"].(map[string]interface{})
+	if !ok {
+		return nil, errors.New(ErrInvalidJWKField)
+	}
+
+	jwkMap, ok := cnf["jwk"].(map[string]interface{})
+	if !ok {
+		return nil, errors.New(ErrInvalidJWKField)
+	}
+
+	jwk := &JWK{}
+	if x, ok := jwkMap["x"].(string); ok {
+		jwk.X = x
+	} else {
+		return nil, errors.New("missing or invalid 'x' field in JWK")
+	}
+
+	if y, ok := jwkMap["y"].(string); ok {
+		jwk.Y = y
+	} else {
+		return nil, errors.New("missing or invalid 'y' field in JWK")
+	}
+
+	if crv, ok := jwkMap["crv"].(string); ok {
+		jwk.Crv = crv
+	} else {
+		return nil, errors.New("missing or invalid 'crv' field in JWK")
+	}
+
+	if kty, ok := jwkMap["kty"].(string); ok {
+		jwk.Kty = kty
+	} else {
+		return nil, errors.New("missing or invalid 'kty' field in JWK")
+	}
+
+	return jwk, nil
+}
+
+func createPubKey(jwk *JWK) (*ecdsa.PublicKey, error) {
+	xBytes, err := base64.RawURLEncoding.DecodeString(jwk.X)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding x: %w", err)
+	}
+
+	yBytes, err := base64.RawURLEncoding.DecodeString(jwk.Y)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding y: %w", err)
 	}
 
 	pubKey := &ecdsa.PublicKey{
@@ -100,84 +196,32 @@ func (c *Client) VerifyCredential(ctx context.Context, request *VerifyCredential
 		Y:     new(big.Int).SetBytes(yBytes),
 	}
 
-	token, err := jwt.Parse(request.Jwt, func(token *jwt.Token) (any, error) {
+	return pubKey, nil
+}
+
+func parseJWT(completeJWT string, pubKey *ecdsa.PublicKey) (*jwt.Token, error) {
+	return jwt.Parse(completeJWT, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodECDSA); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 		return pubKey, nil
 	})
-
-	c.log.Debug("jwt", "token", token)
-
-	if err != nil {
-		msg := "Error verifying token"
-		c.log.Debug(msg, "err", err)
-		return &VerifyCredentialReply{Valid: false, Message: msg}, nil
-	}
-
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		if err := validateClaims(claims); err != nil {
-			msg := "Invalid claims"
-			c.log.Debug(msg, "err", err)
-			return &VerifyCredentialReply{Valid: false, Message: msg}, nil
-		}
-
-		c.log.Debug("Token is valid. Claims:")
-		for key, val := range claims {
-			c.log.Debug("claim", "key", key, "val", val)
-		}
-		return &VerifyCredentialReply{Valid: true}, nil
-	}
-
-	msg := "Invalid token"
-	c.log.Debug(msg, "err", err)
-	return &VerifyCredentialReply{Valid: false, Message: msg}, nil
-}
-
-func splitJWT(jwt string) (*JWTParts, error) {
-	parts := strings.Split(jwt, ".")
-	if len(parts) != 3 {
-		return nil, errors.New("invalid JWT structure, expected three parts")
-	}
-
-	header, err := decodeBase64URL(parts[0])
-	if err != nil {
-		return nil, err
-	}
-
-	payload, err := decodeBase64URL(parts[1])
-	if err != nil {
-		return nil, err
-	}
-
-	signature := parts[2]
-
-	return &JWTParts{
-		Header:    header,
-		Payload:   payload,
-		Signature: signature,
-	}, nil
 }
 
 func validateClaims(claims jwt.MapClaims) error {
 	if exp, ok := claims["exp"].(float64); ok {
 		if time.Now().Unix() > int64(exp) {
-			return errors.New("token has expired")
+			return errors.New(ErrExpiredToken)
 		}
 	}
 
 	if nbf, ok := claims["nbf"].(float64); ok {
 		if time.Now().Unix() < int64(nbf) {
-			return errors.New("token is not valid yet")
+			return errors.New(ErrNotYetValidToken)
 		}
 	}
 
-	//if iss, ok := claims["iss"].(string); ok {
-	//	if iss != "https://issuer.sunet.se" { //TODO: where do I find list of trusted issuers?
-	//		return fmt.Errorf("unexpected issuer: %s", iss)
-	//	}
-	//}
-
+	//TODO(mk): validate more claims here ...
 	return nil
 }
 

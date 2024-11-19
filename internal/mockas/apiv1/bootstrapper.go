bdb4b22e942f4993990e93bc95f2db1737b165d5
masv3971 <masv@sunet.se>
2024-11-19 09:18:08 +0100

    Bootstrapping of documents from Katharina Hilmar WP6.

--- internal/mockas/apiv1/bootstrapper.go
+++ internal/mockas/apiv1/bootstrapper.go
@@ -12,37 +12,44 @@ import (
 )
 
 type person struct {
-	authenticSourcePersonID string
-	firstName               string
-	lastName                string
-	dateOfBirth             string
-	socialSecurityPin       string
-	nationality             []string
-	typeOfEmployment        string
-	workAddress             address
-	employmentAddress       address
+	AuthenticSourcePersonID string      `json:"authentic_source_person_id,omitempty"`
+	FirstName               string      `json:"first_name,omitempty"`
+	LastName                string      `json:"last_name,omitempty"`
+	DateOfBirth             string      `json:"date_of_birth,omitempty"`
+	SocialSecurityPin       string      `json:"social_security_pin,omitempty"`
+	Nationality             []string    `json:"national,omitempty"`
+	TypeOfEmployment        string      `json:"type_of_employment,omitempty"`
+	WorkAddress             *address    `json:"work_address,omitempty"`
+	EmploymentAddress       *address    `json:"employment_address,omitempty"`
+	Ehic                    *personEhic `json:"ehic,omitempty"`
+}
+
+type personEhic struct {
+	CardNumber string `json:"card_number,omitempty"`
+	StartDate  string `json:"start_date,omitempty"`
+	EndDate    string `json:"end_date,omitempty"`
 }
 
 type address struct {
-	Street   string
-	PostCode string
-	Town     string
-	Country  string
+	Street   string `json:"street,omitempty"`
+	PostCode string `json:"post_code,omitempty"`
+	Town     string `json:"town,omitempty"`
+	Country  string `json:"country,omitempty"`
 }
 
 func (p *person) bootstrapPDA1() (map[string]any, error) {
 	doc := pda1.Document{
-		SocialSecurityPin: p.socialSecurityPin,
-		Nationality:       p.nationality,
+		SocialSecurityPin: p.SocialSecurityPin,
+		Nationality:       p.Nationality,
 		DetailsOfEmployment: []pda1.DetailsOfEmployment{
 			{
 				TypeOfEmployment: "01",
 				Name:             "Corp inc.",
 				Address: pda1.AddressWithCountry{
-					Street:   p.employmentAddress.Street,
-					PostCode: p.employmentAddress.PostCode,
-					Town:     p.employmentAddress.Town,
-					Country:  p.employmentAddress.Country,
+					Street:   p.EmploymentAddress.Street,
+					PostCode: p.EmploymentAddress.PostCode,
+					Town:     p.EmploymentAddress.Town,
+					Country:  p.EmploymentAddress.Country,
 				},
 				IDsOfEmployer: []pda1.IDsOfEmployer{
 					{
@@ -55,11 +62,11 @@ func (p *person) bootstrapPDA1() (map[string]any, error) {
 		PlacesOfWork: []pda1.PlacesOfWork{
 			{
 				AFixedPlaceOfWorkExist: false,
-				CountryWork:            p.workAddress.Country,
+				CountryWork:            p.WorkAddress.Country,
 				PlaceOfWork: []pda1.PlaceOfWork{
 					{
-						CompanyVesselName: fmt.Sprintf("vessel_name_%s", strings.ToLower(p.nationality[0])),
-						FlagStateHomeBase: p.workAddress.Country,
+						CompanyVesselName: fmt.Sprintf("vessel_name_%s", strings.ToLower(p.Nationality[0])),
+						FlagStateHomeBase: p.WorkAddress.Country,
 						IDsOfCompany: []pda1.IDsOfCompany{
 							{
 								CompanyID: "3615c840",
@@ -67,16 +74,16 @@ func (p *person) bootstrapPDA1() (map[string]any, error) {
 							},
 						},
 						Address: pda1.Address{
-							Street:   p.workAddress.Street,
-							PostCode: p.workAddress.PostCode,
-							Town:     p.workAddress.Town,
+							Street:   p.WorkAddress.Street,
+							PostCode: p.WorkAddress.PostCode,
+							Town:     p.WorkAddress.Town,
 						},
 					},
 				},
 			},
 		},
 		DecisionLegislationApplicable: pda1.DecisionLegislationApplicable{
-			MemberStateWhichLegislationApplies: p.workAddress.Country,
+			MemberStateWhichLegislationApplies: p.WorkAddress.Country,
 			TransitionalRuleApply:              false,
 			StartingDate:                       "1970-01-01",
 			EndingDate:                         "2038-01-19",
@@ -84,9 +91,9 @@ func (p *person) bootstrapPDA1() (map[string]any, error) {
 		StatusConfirmation:           "01",
 		UniqueNumberOfIssuedDocument: "asd123",
 		CompetentInstitution: pda1.CompetentInstitution{
-			InstitutionID:   fmt.Sprintf("%s:1234", p.nationality[0]),
-			InstitutionName: fmt.Sprintf("institution_name_%s", strings.ToLower(p.nationality[0])),
-			CountryCode:     p.workAddress.Country,
+			InstitutionID:   fmt.Sprintf("%s:1234", p.Nationality[0]),
+			InstitutionName: fmt.Sprintf("institution_name_%s", strings.ToLower(p.Nationality[0])),
+			CountryCode:     p.WorkAddress.Country,
 		},
 	}
 
@@ -96,20 +103,20 @@ func (p *person) bootstrapPDA1() (map[string]any, error) {
 func (p *person) bootstrapEHIC() (map[string]any, error) {
 	doc := ehic.Document{
 		Subject: ehic.Subject{
-			Forename:    p.firstName,
-			FamilyName:  p.lastName,
-			DateOfBirth: p.dateOfBirth,
+			Forename:    p.FirstName,
+			FamilyName:  p.LastName,
+			DateOfBirth: p.DateOfBirth,
 		},
-		SocialSecurityPin: p.socialSecurityPin,
+		SocialSecurityPin: p.SocialSecurityPin,
 		PeriodEntitlement: ehic.PeriodEntitlement{
 			StartingDate: "1970-01-01",
 			EndingDate:   "2038-01-19",
 		},
-		DocumentID: fmt.Sprintf("document_id_%s", p.authenticSourcePersonID),
+		DocumentID: fmt.Sprintf("document_id_%s", p.AuthenticSourcePersonID),
 		CompetentInstitution: ehic.CompetentInstitution{
-			InstitutionID:      fmt.Sprintf("%s:1234", p.workAddress.Country),
-			InstitutionName:    fmt.Sprintf("institution_name_%s", strings.ToLower(p.nationality[0])),
-			InstitutionCountry: p.workAddress.Country,
+			InstitutionID:      fmt.Sprintf("%s:1234", p.WorkAddress.Country),
+			InstitutionName:    fmt.Sprintf("institution_name_%s", strings.ToLower(p.Nationality[0])),
+			InstitutionCountry: p.WorkAddress.Country,
 		},
 	}
 	return doc.Marshal()
@@ -119,117 +126,117 @@ func (p *person) bootstrapEHIC() (map[string]any, error) {
 var persons = map[string][]person{
 	"EHIC": {
 		{
-			authenticSourcePersonID: "10",
-			firstName:               "Carlos",
-			lastName:                "Castaneda",
-			dateOfBirth:             "1970-01-10",
-			socialSecurityPin:       "12345",
-			nationality:             []string{"SE"},
-			typeOfEmployment:        "01",
-			workAddress:             address{Street: "Drottninggatan 1", PostCode: "12345", Town: "Stockholm", Country: "SE"},
-			employmentAddress:       address{Street: "Kungsgatan 1", PostCode: "12346", Town: "Stockholm", Country: "SE"},
+			AuthenticSourcePersonID: "10",
+			FirstName:               "Carlos",
+			LastName:                "Castaneda",
+			DateOfBirth:             "1970-01-10",
+			SocialSecurityPin:       "12345",
+			Nationality:             []string{"SE"},
+			TypeOfEmployment:        "01",
+			WorkAddress:             &address{Street: "Drottninggatan 1", PostCode: "12345", Town: "Stockholm", Country: "SE"},
+			EmploymentAddress:       &address{Street: "Kungsgatan 1", PostCode: "12346", Town: "Stockholm", Country: "SE"},
 		},
 		{
 
-			authenticSourcePersonID: "11",
-			firstName:               "Lenna",
-			lastName:                "Howell",
-			dateOfBirth:             "1935-02-21",
-			socialSecurityPin:       "12357",
-			nationality:             []string{"AT"},
-			typeOfEmployment:        "01",
-			workAddress:             address{Street: "Obere Hauptstraße 1", PostCode: "12346", Town: "Wien", Country: "AT"},
-			employmentAddress:       address{Street: "Untere Hauptstraße 1", PostCode: "12347", Town: "Wien", Country: "AT"},
+			AuthenticSourcePersonID: "11",
+			FirstName:               "Lenna",
+			LastName:                "Howell",
+			DateOfBirth:             "1935-02-21",
+			SocialSecurityPin:       "12357",
+			Nationality:             []string{"AT"},
+			TypeOfEmployment:        "01",
+			WorkAddress:             &address{Street: "Obere Hauptstraße 1", PostCode: "12346", Town: "Wien", Country: "AT"},
+			EmploymentAddress:       &address{Street: "Untere Hauptstraße 1", PostCode: "12347", Town: "Wien", Country: "AT"},
 		},
 		{
-			authenticSourcePersonID: "12",
-			firstName:               "Ute",
-			lastName:                "Anderson",
-			dateOfBirth:             "1967-03-21",
-			socialSecurityPin:       "98883123",
-			nationality:             []string{"DE"},
-			typeOfEmployment:        "02",
-			workAddress:             address{Street: "Vurfelser Kaule 31", PostCode: "51427", Town: "Bergisch Gladbach", Country: "DE"},
-			employmentAddress:       address{Street: "Hieronymusgasse 2", PostCode: "78462", Town: "Konstanz", Country: "DE"},
+			AuthenticSourcePersonID: "12",
+			FirstName:               "Ute",
+			LastName:                "Anderson",
+			DateOfBirth:             "1967-03-21",
+			SocialSecurityPin:       "98883123",
+			Nationality:             []string{"DE"},
+			TypeOfEmployment:        "02",
+			WorkAddress:             &address{Street: "Vurfelser Kaule 31", PostCode: "51427", Town: "Bergisch Gladbach", Country: "DE"},
+			EmploymentAddress:       &address{Street: "Hieronymusgasse 2", PostCode: "78462", Town: "Konstanz", Country: "DE"},
 		},
 		{
-			authenticSourcePersonID: "13",
-			firstName:               "Olivia",
-			lastName:                "Eelman",
-			dateOfBirth:             "1971-03-13",
-			socialSecurityPin:       "097428358",
-			nationality:             []string{"NL"},
-			typeOfEmployment:        "02",
-			workAddress:             address{Street: "Jansplein 49", PostCode: "6811 GD", Town: "Arnhem", Country: "NL"},
-			employmentAddress:       address{Street: "Buytenparklaan 30", PostCode: "2717 AX", Town: "Zoetermeer", Country: "NL"},
+			AuthenticSourcePersonID: "13",
+			FirstName:               "Olivia",
+			LastName:                "Eelman",
+			DateOfBirth:             "1971-03-13",
+			SocialSecurityPin:       "097428358",
+			Nationality:             []string{"NL"},
+			TypeOfEmployment:        "02",
+			WorkAddress:             &address{Street: "Jansplein 49", PostCode: "6811 GD", Town: "Arnhem", Country: "NL"},
+			EmploymentAddress:       &address{Street: "Buytenparklaan 30", PostCode: "2717 AX", Town: "Zoetermeer", Country: "NL"},
 		},
 		{
-			authenticSourcePersonID: "14",
-			firstName:               "Patrick",
-			lastName:                "Høgh-Nørgaard Iversen",
-			dateOfBirth:             "1994-03-07",
-			socialSecurityPin:       "449-49-2795",
-			nationality:             []string{"DK"},
-			typeOfEmployment:        "02",
-			workAddress:             address{Street: "Nørregade 7", PostCode: "1165", Town: "København", Country: "DK"},
-			employmentAddress:       address{Street: "Galgebakkevej 3", PostCode: "2630", Town: "Taastrup", Country: "DK"},
+			AuthenticSourcePersonID: "14",
+			FirstName:               "Patrick",
+			LastName:                "Høgh-Nørgaard Iversen",
+			DateOfBirth:             "1994-03-07",
+			SocialSecurityPin:       "449-49-2795",
+			Nationality:             []string{"DK"},
+			TypeOfEmployment:        "02",
+			WorkAddress:             &address{Street: "Nørregade 7", PostCode: "1165", Town: "København", Country: "DK"},
+			EmploymentAddress:       &address{Street: "Galgebakkevej 3", PostCode: "2630", Town: "Taastrup", Country: "DK"},
 		},
 	},
 	"PDA1": {
 		{
-			authenticSourcePersonID: "20",
-			firstName:               "Mats",
-			lastName:                "Christiansen",
-			dateOfBirth:             "1983-03-27",
-			socialSecurityPin:       "98123",
-			nationality:             []string{"DK"},
-			typeOfEmployment:        "02",
-			workAddress:             address{Street: "Møllestien 2", PostCode: "12332", Town: "Aarhus", Country: "DK"},
-			employmentAddress:       address{Street: "Møllestien 2", PostCode: "12332", Town: "Aarhus", Country: "DK"},
+			AuthenticSourcePersonID: "20",
+			FirstName:               "Mats",
+			LastName:                "Christiansen",
+			DateOfBirth:             "1983-03-27",
+			SocialSecurityPin:       "98123",
+			Nationality:             []string{"DK"},
+			TypeOfEmployment:        "02",
+			WorkAddress:             &address{Street: "Møllestien 2", PostCode: "12332", Town: "Aarhus", Country: "DK"},
+			EmploymentAddress:       &address{Street: "Møllestien 2", PostCode: "12332", Town: "Aarhus", Country: "DK"},
 		},
 		{
-			authenticSourcePersonID: "21",
-			firstName:               "Aldrich",
-			lastName:                "Derichs",
-			dateOfBirth:             "1971-05-25",
-			socialSecurityPin:       "98123123",
-			nationality:             []string{"DE"},
-			typeOfEmployment:        "02",
-			workAddress:             address{Street: "Petzoldstrasse 2", PostCode: "03042", Town: "Cottbus", Country: "DE"},
-			employmentAddress:       address{Street: "Masurenallee 33", PostCode: "47055", Town: "Duisburg", Country: "DE"},
+			AuthenticSourcePersonID: "21",
+			FirstName:               "Aldrich",
+			LastName:                "Derichs",
+			DateOfBirth:             "1971-05-25",
+			SocialSecurityPin:       "98123123",
+			Nationality:             []string{"DE"},
+			TypeOfEmployment:        "02",
+			WorkAddress:             &address{Street: "Petzoldstrasse 2", PostCode: "03042", Town: "Cottbus", Country: "DE"},
+			EmploymentAddress:       &address{Street: "Masurenallee 33", PostCode: "47055", Town: "Duisburg", Country: "DE"},
 		},
 		{
-			authenticSourcePersonID: "22",
-			firstName:               "Algot",
-			lastName:                "Holmberg",
-			dateOfBirth:             "1955-11-25",
-			socialSecurityPin:       "12345",
-			nationality:             []string{"SE"},
-			typeOfEmployment:        "01",
-			workAddress:             address{Street: "Idrottsgatan 2", PostCode: "753 33", Town: "Uppsala", Country: "SE"},
-			employmentAddress:       address{Street: "Östra Storgatan 10A", PostCode: "611 34", Town: "Nyköping", Country: "SE"},
+			AuthenticSourcePersonID: "22",
+			FirstName:               "Algot",
+			LastName:                "Holmberg",
+			DateOfBirth:             "1955-11-25",
+			SocialSecurityPin:       "12345",
+			Nationality:             []string{"SE"},
+			TypeOfEmployment:        "01",
+			WorkAddress:             &address{Street: "Idrottsgatan 2", PostCode: "753 33", Town: "Uppsala", Country: "SE"},
+			EmploymentAddress:       &address{Street: "Östra Storgatan 10A", PostCode: "611 34", Town: "Nyköping", Country: "SE"},
 		},
 		{
-			authenticSourcePersonID: "23",
-			firstName:               "Joep",
-			lastName:                "Cicilia",
-			dateOfBirth:             "1999-07-29",
-			socialSecurityPin:       "753841605",
-			nationality:             []string{"NL"},
-			typeOfEmployment:        "01",
-			workAddress:             address{Street: "Het Rond 6", PostCode: "3701 HS", Town: "Zeist", Country: "NL"},
-			employmentAddress:       address{Street: "Oude Ebbingestraat 68", PostCode: "9712 HM", Town: "Groningen", Country: "NL"},
+			AuthenticSourcePersonID: "23",
+			FirstName:               "Joep",
+			LastName:                "Cicilia",
+			DateOfBirth:             "1999-07-29",
+			SocialSecurityPin:       "753841605",
+			Nationality:             []string{"NL"},
+			TypeOfEmployment:        "01",
+			WorkAddress:             &address{Street: "Het Rond 6", PostCode: "3701 HS", Town: "Zeist", Country: "NL"},
+			EmploymentAddress:       &address{Street: "Oude Ebbingestraat 68", PostCode: "9712 HM", Town: "Groningen", Country: "NL"},
 		},
 		{
-			authenticSourcePersonID: "24",
-			firstName:               "Hollis",
-			lastName:                "Hoeger",
-			dateOfBirth:             "1983-05-05",
-			socialSecurityPin:       "315-95-2501",
-			nationality:             []string{"AT"},
-			typeOfEmployment:        "01",
-			workAddress:             address{Street: "Stumpergasse 48/8", PostCode: "1060", Town: "Wien", Country: "AT"},
-			employmentAddress:       address{Street: "Franz-Josef-Platz 3", PostCode: "4810", Town: "Gmunden", Country: "AT"},
+			AuthenticSourcePersonID: "24",
+			FirstName:               "Hollis",
+			LastName:                "Hoeger",
+			DateOfBirth:             "1983-05-05",
+			SocialSecurityPin:       "315-95-2501",
+			Nationality:             []string{"AT"},
+			TypeOfEmployment:        "01",
+			WorkAddress:             &address{Street: "Stumpergasse 48/8", PostCode: "1060", Town: "Wien", Country: "AT"},
+			EmploymentAddress:       &address{Street: "Franz-Josef-Platz 3", PostCode: "4810", Town: "Gmunden", Country: "AT"},
 		},
 	},
 }
@@ -256,13 +263,13 @@ func (c *Client) bootstrapperConstructor(ctx context.Context) error {
 				}
 			}
 			meta := &model.MetaData{
-				AuthenticSource: fmt.Sprintf("authentic_source_%s", strings.ToLower(p.nationality[0])),
+				AuthenticSource: fmt.Sprintf("authentic_source_%s", strings.ToLower(p.Nationality[0])),
 				DocumentType:    documentType,
-				DocumentID:      fmt.Sprintf("document_id_%s", p.authenticSourcePersonID),
+				DocumentID:      fmt.Sprintf("document_id_%s", p.AuthenticSourcePersonID),
 				DocumentVersion: "1.0.0",
 				RealData:        false,
 				Collect: &model.Collect{
-					ID:         fmt.Sprintf("collect_id_%s", p.authenticSourcePersonID),
+					ID:         fmt.Sprintf("collect_id_%s", p.AuthenticSourcePersonID),
 					ValidUntil: 2147520172,
 				},
 				CredentialValidFrom: 1,
@@ -271,23 +278,23 @@ func (c *Client) bootstrapperConstructor(ctx context.Context) error {
 					ID:      "9da40dc0",
 					Revoked: false,
 					Reference: model.RevocationReference{
-						AuthenticSource: fmt.Sprintf("authentic_source_%s", strings.ToLower(p.nationality[0])),
+						AuthenticSource: fmt.Sprintf("authentic_source_%s", strings.ToLower(p.Nationality[0])),
 						DocumentType:    documentType,
-						DocumentID:      fmt.Sprintf("document_id_%s", p.authenticSourcePersonID),
+						DocumentID:      fmt.Sprintf("document_id_%s", p.AuthenticSourcePersonID),
 					},
 				},
 			}
 
 			identities := []model.Identity{
 				{
-					AuthenticSourcePersonID: p.authenticSourcePersonID,
+					AuthenticSourcePersonID: p.AuthenticSourcePersonID,
 					Schema: &model.IdentitySchema{
-						Name:    p.workAddress.Country,
+						Name:    p.WorkAddress.Country,
 						Version: "1.0.0",
 					},
-					FamilyName: p.lastName,
-					GivenName:  p.firstName,
-					BirthDate:  p.dateOfBirth,
+					FamilyName: p.LastName,
+					GivenName:  p.FirstName,
+					BirthDate:  p.DateOfBirth,
 				},
 			}
 
@@ -335,5 +342,26 @@ func (c *Client) bootstrapper(ctx context.Context) error {
 		}
 	}
 
+	for _, mockUpload := range c.parisMocks {
+		c.log.Debug("uploading paris mock", "authentic_source_person_id", mockUpload.Meta.DocumentType)
+
+		document := &uploadMock{
+			Meta:                mockUpload.Meta,
+			Identities:          mockUpload.Identities,
+			DocumentDisplay:     mockUpload.DocumentDisplay,
+			DocumentData:        mockUpload.DocumentData,
+			DocumentDataVersion: mockUpload.DocumentDataVersion,
+		}
+
+		resp, err := c.uploader(ctx, document)
+		if err != nil {
+			c.log.Error(err, "failed to upload", "mockUpload", mockUpload)
+			return err
+		}
+
+		if resp.StatusCode != 200 {
+			return errors.New("upload failed")
+		}
+	}
 	return nil
 }

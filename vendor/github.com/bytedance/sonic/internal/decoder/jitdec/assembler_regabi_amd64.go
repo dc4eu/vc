2d148bbebdac6e3a707212051d8c8f145e459787
Magnus Svensson <masv@sunet.se>
2025-03-11 15:00:41 +0100

    Vendor and deps.

--- vendor/github.com/bytedance/sonic/internal/decoder/jitdec/assembler_regabi_amd64.go
+++ vendor/github.com/bytedance/sonic/internal/decoder/jitdec/assembler_regabi_amd64.go
@@ -1,5 +1,5 @@
-//go:build go1.17 && !go1.24
-// +build go1.17,!go1.24
+//go:build go1.17 && !go1.25
+// +build go1.17,!go1.25
 
 /*
  * Copyright 2021 ByteDance Inc.
@@ -67,7 +67,7 @@ import (
  */
 
 const (
-    _FP_args   = 72     // 72 bytes to pass and spill register arguements
+    _FP_args   = 72     // 72 bytes to pass and spill register arguments
     _FP_fargs  = 80     // 80 bytes for passing arguments to other Go functions
     _FP_saves  = 48     // 48 bytes for saving the registers before CALL instructions
     _FP_locals = 144    // 144 bytes for local variables
@@ -203,9 +203,9 @@ var (
 var _VAR_fl = jit.Ptr(_SP, _FP_fargs + _FP_saves + 112)
 
 var (
-    _VAR_et = jit.Ptr(_SP, _FP_fargs + _FP_saves + 120) // save dismatched type
+    _VAR_et = jit.Ptr(_SP, _FP_fargs + _FP_saves + 120) // save mismatched type
     _VAR_pc = jit.Ptr(_SP, _FP_fargs + _FP_saves + 128) // save skip return pc
-    _VAR_ic = jit.Ptr(_SP, _FP_fargs + _FP_saves + 136) // save dismatched position
+    _VAR_ic = jit.Ptr(_SP, _FP_fargs + _FP_saves + 136) // save mismatched position
 )
 
 type _Assembler struct {
@@ -313,9 +313,10 @@ var _OpFuncTab = [256]func(*_Assembler, *_Instr) {
     _OP_check_char_0     : (*_Assembler)._asm_OP_check_char_0,
     _OP_dismatch_err     : (*_Assembler)._asm_OP_dismatch_err,
     _OP_go_skip          : (*_Assembler)._asm_OP_go_skip,
-    _OP_skip_emtpy         : (*_Assembler)._asm_OP_skip_empty,
+    _OP_skip_emtpy       : (*_Assembler)._asm_OP_skip_empty,
     _OP_add              : (*_Assembler)._asm_OP_add,
     _OP_check_empty      : (*_Assembler)._asm_OP_check_empty,
+    _OP_unsupported      : (*_Assembler)._asm_OP_unsupported,
     _OP_debug            : (*_Assembler)._asm_OP_debug,
 }
 
@@ -456,7 +457,7 @@ func (self *_Assembler) call_vf(fn obj.Addr) {
 /** Assembler Error Handlers **/
 
 var (
-    _F_convT64        = jit.Func(convT64)
+    _F_convT64        = jit.Func(rt.ConvT64)
     _F_error_wrap     = jit.Func(error_wrap)
     _F_error_type     = jit.Func(error_type)
     _F_error_field    = jit.Func(error_field)
@@ -483,6 +484,7 @@ var (
     _V_stackOverflow              = jit.Imm(int64(uintptr(unsafe.Pointer(&stackOverflow))))
     _I_json_UnsupportedValueError = jit.Itab(_T_error, reflect.TypeOf(new(json.UnsupportedValueError)))
     _I_json_MismatchTypeError     = jit.Itab(_T_error, reflect.TypeOf(new(MismatchTypeError)))
+    _I_json_MismatchQuotedError   = jit.Itab(_T_error, reflect.TypeOf(new(MismatchQuotedError)))
 )
 
 func (self *_Assembler) type_error() {
@@ -661,7 +663,7 @@ func (self *_Assembler) skip_key_value() {
 
 var (
     _T_byte     = jit.Type(byteType)
-    _F_mallocgc = jit.Func(mallocgc)
+    _F_mallocgc = jit.Func(rt.Mallocgc)
 )
 
 func (self *_Assembler) malloc_AX(nb obj.Addr, ret obj.Addr) {
@@ -963,8 +965,8 @@ func (self *_Assembler) unquote_twice(p obj.Addr, n obj.Addr, stack bool) {
 /** Memory Clearing Routines **/
 
 var (
-    _F_memclrHasPointers    = jit.Func(memclrHasPointers)
-    _F_memclrNoHeapPointers = jit.Func(memclrNoHeapPointers)
+    _F_memclrHasPointers    = jit.Func(rt.MemclrHasPointers)
+    _F_memclrNoHeapPointers = jit.Func(rt.MemclrNoHeapPointers)
 )
 
 func (self *_Assembler) mem_clear_fn(ptrfree bool) {
@@ -988,10 +990,10 @@ func (self *_Assembler) mem_clear_rem(size int64, ptrfree bool) {
 /** Map Assigning Routines **/
 
 var (
-    _F_mapassign           = jit.Func(mapassign)
-    _F_mapassign_fast32    = jit.Func(mapassign_fast32)
-    _F_mapassign_faststr   = jit.Func(mapassign_faststr)
-    _F_mapassign_fast64ptr = jit.Func(mapassign_fast64ptr)
+    _F_mapassign           = jit.Func(rt.Mapassign)
+    _F_mapassign_fast32    = jit.Func(rt.Mapassign_fast32)
+    _F_mapassign_faststr   = jit.Func(rt.Mapassign_faststr)
+    _F_mapassign_fast64ptr = jit.Func(rt.Mapassign_fast64ptr)
 )
 
 var (
@@ -1129,15 +1131,19 @@ func (self *_Assembler) unmarshal_func(t reflect.Type, fn obj.Addr, deref bool) 
     self.Emit("MOVQ" , _ARG_sv_n, _DI)          // MOVQ    sv.n, DI
     self.call_go(fn)                            // CALL_GO ${fn}
     self.Emit("TESTQ", _ET, _ET)                // TESTQ   ET, ET
-    self.Sjmp("JZ"  , "_unmarshal_func_end_{n}")               // JNZ     _error
-    self.Emit("MOVQ", _I_json_MismatchTypeError, _CX)             // MOVQ    ET, VAR.et
-    self.Emit("CMPQ", _ET, _CX)          // check if MismatchedError
-    self.Sjmp("JNE" , _LB_error)                
-    self.Emit("MOVQ", jit.Type(t), _CX)        // store current type 
-    self.Emit("MOVQ", _CX, _VAR_et)             // store current type 
-    self.Emit("MOVQ", _VAR_ic, _IC)             // recover the pos
-    self.Emit("XORL", _ET, _ET)
-    self.Link("_unmarshal_func_end_{n}")
+    if fn == _F_decodeJsonUnmarshalerQuoted {
+        self.Sjmp("JZ"  , "_unmarshal_func_end_{n}")            // JZ   _unmarshal_func_end_{n}
+        self.Emit("MOVQ", _I_json_MismatchQuotedError, _CX)     // MOVQ _I_json_MismatchQuotedError, CX
+        self.Emit("CMPQ", _ET, _CX)            // check if MismatchQuotedError
+        self.Sjmp("JNE" , _LB_error)           // JNE     _error
+        self.Emit("MOVQ", jit.Type(t), _CX)    // store current type 
+        self.Emit("MOVQ", _CX, _VAR_et)        // store current type as mismatched type
+        self.Emit("MOVQ", _VAR_ic, _IC)        // recover the pos at mismatched, continue to parse
+        self.Emit("XORL", _ET, _ET)            // clear ET
+        self.Link("_unmarshal_func_end_{n}")
+    } else {
+        self.Sjmp("JNE" , _LB_error)           // JNE     _error
+    }
 }
 
 /** Dynamic Decoding Routine **/
@@ -1178,12 +1184,12 @@ func (self *_Assembler) decode_dynamic(vt obj.Addr, vp obj.Addr) {
 /** OpCode Assembler Functions **/
 
 var (
-    _F_memequal         = jit.Func(memequal)
-    _F_memmove          = jit.Func(memmove)
+    _F_memequal         = jit.Func(rt.MemEqual)
+    _F_memmove          = jit.Func(rt.Memmove)
     _F_growslice        = jit.Func(rt.GrowSlice)
-    _F_makeslice        = jit.Func(makeslice)
-    _F_makemap_small    = jit.Func(makemap_small)
-    _F_mapassign_fast64 = jit.Func(mapassign_fast64)
+    _F_makeslice        = jit.Func(rt.MakeSliceStd)
+    _F_makemap_small    = jit.Func(rt.MakemapSmall)
+    _F_mapassign_fast64 = jit.Func(rt.Mapassign_fast64)
 )
 
 var (
@@ -1192,7 +1198,7 @@ var (
 )
 
 var (
-    _F_b64decode   = jit.Imm(int64(_subr__b64decode))
+    _F_b64decode   = jit.Imm(int64(rt.SubrB64Decode))
     _F_decodeValue = jit.Imm(int64(_subr_decode_value))
 )
 
@@ -1258,6 +1264,11 @@ func (self *_Assembler) _asm_OP_dyn(p *_Instr) {
     self.Emit("LEAQ"   , jit.Ptr(_VP, 8), _DI)              // LEAQ    8(VP), DI
     self.decode_dynamic(_CX, _DI)                           // DECODE  CX, DI
     self.Link("_decode_end_{n}")                            // _decode_end_{n}:
+}
+
+func (self *_Assembler) _asm_OP_unsupported(p *_Instr) {
+    self.Emit("MOVQ", jit.Type(p.vt()), _ET)               // MOVQ    ${p.vt()}, ET
+    self.Sjmp("JMP" , _LB_type_error)                      // JMP     _LB_type_error
 }
 
 func (self *_Assembler) _asm_OP_str(_ *_Instr) {
@@ -1356,7 +1367,7 @@ func (self *_Assembler) _asm_OP_num(_ *_Instr) {
     self.Emit("MOVQ", _R9, _VAR_pc)
     self.Sjmp("JMP"  , _LB_skip_one)
 
-    /* assgin string */
+    /* assign string */
     self.Link("_num_next_{n}")
     self.slice_from_r(_AX, 0)
     self.Emit("BTQ", jit.Imm(_F_copy_string), _ARG_fv)
@@ -1542,7 +1553,7 @@ func (self *_Assembler) _asm_OP_map_key_i32(p *_Instr) {
     self.parse_signed(int32Type, "", p.vi())                                                     // PARSE     int32
     self.range_signed_CX(_I_int32, _T_int32, math.MinInt32, math.MaxInt32)     // RANGE     int32
     self.match_char('"')
-    if vt := p.vt(); !mapfast(vt) {
+    if vt := p.vt(); !rt.IsMapfast(vt) {
         self.mapassign_std(vt, _VAR_st_Iv)                                  // MAPASSIGN int32, mapassign, st.Iv
     } else {
         self.Emit("MOVQ", _CX, _AX)                                         // MOVQ CX, AX
@@ -1553,7 +1564,7 @@ func (self *_Assembler) _asm_OP_map_key_i32(p *_Instr) {
 func (self *_Assembler) _asm_OP_map_key_i64(p *_Instr) {
     self.parse_signed(int64Type, "", p.vi())                                 // PARSE     int64
     self.match_char('"')
-    if vt := p.vt(); !mapfast(vt) {
+    if vt := p.vt(); !rt.IsMapfast(vt) {
         self.mapassign_std(vt, _VAR_st_Iv)              // MAPASSIGN int64, mapassign, st.Iv
     } else {
         self.Emit("MOVQ", _VAR_st_Iv, _AX)              // MOVQ      st.Iv, AX
@@ -1579,7 +1590,7 @@ func (self *_Assembler) _asm_OP_map_key_u32(p *_Instr) {
     self.parse_unsigned(uint32Type, "", p.vi())                                       // PARSE     uint32
     self.range_unsigned_CX(_I_uint32, _T_uint32, math.MaxUint32)   // RANGE     uint32
     self.match_char('"')
-    if vt := p.vt(); !mapfast(vt) {
+    if vt := p.vt(); !rt.IsMapfast(vt) {
         self.mapassign_std(vt, _VAR_st_Iv)                      // MAPASSIGN uint32, vt.Iv
     } else {
         self.Emit("MOVQ", _CX, _AX)                             // MOVQ CX, AX
@@ -1590,7 +1601,7 @@ func (self *_Assembler) _asm_OP_map_key_u32(p *_Instr) {
 func (self *_Assembler) _asm_OP_map_key_u64(p *_Instr) {
     self.parse_unsigned(uint64Type, "", p.vi())                                       // PARSE     uint64
     self.match_char('"')
-    if vt := p.vt(); !mapfast(vt) {
+    if vt := p.vt(); !rt.IsMapfast(vt) {
         self.mapassign_std(vt, _VAR_st_Iv)                      // MAPASSIGN uint64, vt.Iv
     } else {
         self.Emit("MOVQ", _VAR_st_Iv, _AX)                      // MOVQ      st.Iv, AX
@@ -1615,7 +1626,7 @@ func (self *_Assembler) _asm_OP_map_key_f64(p *_Instr) {
 func (self *_Assembler) _asm_OP_map_key_str(p *_Instr) {
     self.parse_string()                          // PARSE     STRING
     self.unquote_once(_ARG_sv_p, _ARG_sv_n, true, true)      // UNQUOTE   once, sv.p, sv.n
-    if vt := p.vt(); !mapfast(vt) {
+    if vt := p.vt(); !rt.IsMapfast(vt) {
         self.valloc(vt.Key(), _DI)
         self.Emit("MOVOU", _ARG_sv, _X0)
         self.Emit("MOVOU", _X0, jit.Ptr(_DI, 0))
@@ -1789,6 +1800,8 @@ func (self *_Assembler) _asm_OP_struct_field(p *_Instr) {
     self.Emit("MOVQ" , _R8, _VAR_sr)                            // MOVQ    R8, sr
     self.Sjmp("JMP"  , "_end_{n}")                              // JMP     _end_{n}
     self.Link("_try_lowercase_{n}")                             // _try_lowercase_{n}:
+    self.Emit("BTQ"  , jit.Imm(_F_case_sensitive), _ARG_fv)     // check if enable option CaseSensitive
+    self.Sjmp("JC"   , "_unknown_{n}")                         
     self.Emit("MOVQ" , jit.Imm(referenceFields(p.vf())), _AX)   // MOVQ    ${p.vf()}, AX
     self.Emit("MOVQ", _ARG_sv_p, _BX)                            // MOVQ   sv, BX
     self.Emit("MOVQ", _ARG_sv_n, _CX)                            // MOVQ   sv, CX
@@ -1796,6 +1809,10 @@ func (self *_Assembler) _asm_OP_struct_field(p *_Instr) {
     self.Emit("MOVQ" , _AX, _VAR_sr)                            // MOVQ    AX, _VAR_sr
     self.Emit("TESTQ", _AX, _AX)                                // TESTQ   AX, AX
     self.Sjmp("JNS"  , "_end_{n}")                              // JNS     _end_{n}
+    self.Link("_unknown_{n}")
+    // HACK: because `_VAR_sr` maybe used in `F_vstring`, so we should clear here again for `_OP_switch`.
+    self.Emit("MOVQ" , jit.Imm(-1), _AX)                        // MOVQ    $-1, AX
+    self.Emit("MOVQ" , _AX, _VAR_sr)                            // MOVQ    AX, sr
     self.Emit("BTQ"  , jit.Imm(_F_disable_unknown), _ARG_fv)    // BTQ     ${_F_disable_unknown}, fv
     self.Sjmp("JC"   , _LB_field_error)                         // JC      _field_error
     self.Link("_end_{n}")                                       // _end_{n}:

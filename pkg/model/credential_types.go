24c723a11db811a7481d035619c496704a721231
Magnus Svensson <masv@sunet.se>
2025-06-03 15:37:43 +0200

    Change vct.

--- pkg/model/credential_types.go
+++ pkg/model/credential_types.go
@@ -4,7 +4,7 @@ const (
 	CredentialTypeUrnEudiEhic1            = "urn:eudi:ehic:1"
 	CredentialTypeUrnEudiPda11            = "urn:eudi:pda1:1"
 	CredentialTypeUrnEuEuropaEcEudiPid1   = "urn:eu.europa.ec.eudi:pid:1"
-	CredentialTypeUrnEduiDiploma1         = "urn:edui:diploma:1"
-	CredentialTypeUrnEduiElm1             = "urn:edui:elm:1"
-	CredentialTypeUrnEduiMicroCredential1 = "urn:edui:micro_credential:1"
+	CredentialTypeUrnEduiDiploma1         = "urn:eudi:diploma:1"
+	CredentialTypeUrnEduiElm1             = "urn:eudi:elm:1"
+	CredentialTypeUrnEduiMicroCredential1 = "urn:eudi:micro_credential:1"
 )

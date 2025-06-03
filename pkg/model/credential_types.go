3e1b92592a1ac80581aaea6b6c0931e8f55f02f5
Magnus Svensson <masv@sunet.se>
2025-06-03 16:39:46 +0200

    Change vct for pid.

--- pkg/model/credential_types.go
+++ pkg/model/credential_types.go
@@ -3,7 +3,7 @@ package model
 const (
 	CredentialTypeUrnEudiEhic1            = "urn:eudi:ehic:1"
 	CredentialTypeUrnEudiPda11            = "urn:eudi:pda1:1"
-	CredentialTypeUrnEuEuropaEcEudiPid1   = "urn:eu.europa.ec.eudi:pid:1"
+	CredentialTypeUrnEudiPid1             = "urn:eudi:pid:1"
 	CredentialTypeUrnEduiDiploma1         = "urn:eudi:diploma:1"
 	CredentialTypeUrnEduiElm1             = "urn:eudi:elm:1"
 	CredentialTypeUrnEduiMicroCredential1 = "urn:eudi:micro_credential:1"

bda3eee1395c70bc7fec87d705cff4c187d5c8ed
Magnus Svensson <masv@sunet.se>
2025-06-13 16:28:19 +0200

    Simple tool to sync vctm files to vc-ops repo.

---
+++ developer_tools/sync_vctm_vc-ops.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+files=("diploma" "elm" "microcredential" "pda1" "ehic" "pid")
+
+for file in "${files[@]}"; do
+    printf "\nSyncing vctm_%s.json...\n" "$file"
+    remote="../vc-ops/interop-common/overlay/opt/vc/metadata/vctm_$file.json"
+    local="metadata/vctm_$file.json"
+
+    rsync -avz --progress "$remote" "$local"
+
+    remoteS256=$(sha256sum $remote)
+    localS256=$(sha256sum $local)
+    printf "\nRemote SHA256: %s\nLocal SHA256: %s\n" "$remoteS256" "$localS256"
+done

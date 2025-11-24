368a704c60b9acb3effb6334501e8b35d7409313
Magnus Svensson <masv@sunet.se>
2025-11-24 09:37:10 +0100

    Fix verifier direct_post api.

--- pkg/openid4vp/response_parameters.go
+++ pkg/openid4vp/response_parameters.go
@@ -7,6 +7,11 @@ import (
 	"vc/pkg/sdjwtvc"
 )
 
+type VPResponse struct {
+	VPToken map[string]string `json:"vp_token,omitempty" bson:"vp_token" validate:"required"`
+	State   string            `json:"state,omitempty" bson:"state" validate:"required"`
+}
+
 type ResponseParameters struct {
 	//VPToken REQUIRED. The structure of this parameter depends on the query language used to request the presentations in the Authorization Request:
 	VPToken string `json:"vp_token,omitempty" bson:"vp_token" validate:"omitempty,required_if=ResponseType vp_token"`

3d9c856f301ef11e64ba18f7f32c2df5e48ef983
Magnus Svensson <masv@sunet.se>
2025-11-26 16:46:35 +0100

    Add more credential types.

--- pkg/model/credential_types.go
+++ pkg/model/credential_types.go
@@ -4,6 +4,8 @@ const (
 	CredentialTypeUrnEudiEhic1            = "urn:eudi:ehic:1"             // #nosec G101
 	CredentialTypeUrnEudiPda11            = "urn:eudi:pda1:1"             // #nosec G101
 	CredentialTypeUrnEudiPid1             = "urn:eudi:pid:1"              // #nosec G101
+	CredentialTypeUrnEudiPidARF151        = "urn:eudi:pid:arf-1.5:1"      // #nosec G101
+	CredentialTypeUrnEudiPidARG181        = "urn:eudi:pid:arf-1.8:1"      // #nosec G101
 	CredentialTypeUrnEudiDiploma1         = "urn:eudi:diploma:1"          // #nosec G101
 	CredentialTypeUrnEudiElm1             = "urn:eudi:elm:1"              // #nosec G101
 	CredentialTypeUrnEudiMicroCredential1 = "urn:eudi:micro_credential:1" // #nosec G101
